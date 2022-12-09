06336b89bc7e9b9b234b04acb441bcb907c20d03
TheRealVizard <TheRealVizard@users.noreply.github.com>
2022-12-09 16:20:44 +0000

    pre-commit-fix

--- apps/patient/apps.py
+++ apps/patient/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class PatientConfig(AppConfig):
     """Defines the name for the patient app."""
 
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.patient'
-    verbose_name = 'Paciente'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.patient"
+    verbose_name = "Paciente"
