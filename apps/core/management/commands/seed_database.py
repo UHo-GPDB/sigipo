8107d3825613cd5df7eff0e44ac8cec397ede558
Arlan00101 <Arlan00101@users.noreply.github.com>
2023-01-17 20:45:40 +0000

    fix

--- apps/core/management/commands/seed_database.py
+++ apps/core/management/commands/seed_database.py
@@ -1,42 +1,78 @@
-from datetime import datetime
-from random import randint
-
 from django.core.management.base import BaseCommand
-from apps.patient.factories import *
-from apps.geographic_location.factories import *
-from apps.employee.factories import *
-from apps.classifiers.factories import *
-from apps.cancer_registry.factories import *
-from apps.chemotherapy.factories import *
-from apps.nuclear_medicine.factories import *
+
+from apps.cancer_registry.factories import (
+    DoctorFactory,
+    GroupFactory,
+    MorphologyFactory,
+    NeoplasmFactory,
+    PatientFactory,
+    TopographyFactory,
+)
+from apps.chemotherapy.factories import (
+    CycleFactory,
+    CycleMedicationFactory,
+    DrugFactory,
+    MedicationFactory,
+    ProtocolFactory,
+    SchemeFactory,
+)
+from apps.geographic_location.factories import MunicipalityFactory, ProvinceFactory
+from apps.nuclear_medicine.factories import (
+    GammagraphyFactory,
+    HormonalResultFactory,
+    HormonalStudyFactory,
+    IodineDetectionFactory,
+    NuclearMedicineDrugFactory,
+    OncologicResultFactory,
+    OncologicStudyFactory,
+    RadioIsotopeFactory,
+    SerialIodineDetectionFactory,
+    StudyFactory,
+)
+
 
 class Command(BaseCommand):
     help = "Generates 5 instances of all models"
 
     def handle(self, *args, **options):
-        
-        for i in range(5):
+
+        for _ in range(5):
             province = ProvinceFactory.create()
-            municipality = MunicipalityFactory.create(province = province)
-            patient = PatientFactory.create(residence_municipality = municipality, born_municipality = municipality)
+            municipality = MunicipalityFactory.create(province=province)
+            patient = PatientFactory.create(
+                residence_municipality=municipality, born_municipality=municipality
+            )
             group = GroupFactory.create()
             doctor = DoctorFactory.create(group=group)
             topography = TopographyFactory.create()
             morphology = MorphologyFactory.create()
             study = StudyFactory.create()
             radioisotope = RadioIsotopeFactory.create()
-            neoplasm = NeoplasmFactory.create(patient = patient, primary_site = topography, histologic_type = morphology, group = group, medic_that_report = doctor)
+            NeoplasmFactory.create(
+                patient=patient,
+                primary_site=topography,
+                histologic_type=morphology,
+                group=group,
+                medic_that_report=doctor,
+            )
             scheme = SchemeFactory.create()
-            protocol = ProtocolFactory.create(patient = patient, scheme = scheme, doctor = doctor)
+            protocol = ProtocolFactory.create(
+                patient=patient, scheme=scheme, doctor=doctor
+            )
             drug = DrugFactory.create()
             nuclearmedicinedrug = NuclearMedicineDrugFactory.create()
-            medication = MedicationFactory.create(drug = drug, protocol = protocol)
-            cycle = CycleFactory.create(protocol = protocol)
-            cyclemedication = CycleMedicationFactory.create(cycle = cycle, drug = drug)
-            oncologicstudy = OncologicStudyFactory.create(patient = patient)
-            hormonalstudy = HormonalStudyFactory.create(patient = patient)
-            oncologicresult = OncologicResultFactory.create(oncologic_study = oncologicstudy)
-            hormonalresult = HormonalResultFactory.create(hormonal_study = hormonalstudy)
-            iodinedetection = IodineDetectionFactory.create(patient = patient)
-            serialiodinedetection = SerialIodineDetectionFactory.create(patient = patient)
-            gammagraphy = GammagraphyFactory.create(patient = patient, drug = nuclearmedicinedrug, radio_isotope = radioisotope, requested_study = [study])
+            MedicationFactory.create(drug=drug, protocol=protocol)
+            cycle = CycleFactory.create(protocol=protocol)
+            CycleMedicationFactory.create(cycle=cycle, drug=drug)
+            oncologicstudy = OncologicStudyFactory.create(patient=patient)
+            hormonalstudy = HormonalStudyFactory.create(patient=patient)
+            OncologicResultFactory.create(oncologic_study=oncologicstudy)
+            HormonalResultFactory.create(hormonal_study=hormonalstudy)
+            IodineDetectionFactory.create(patient=patient)
+            SerialIodineDetectionFactory.create(patient=patient)
+            GammagraphyFactory.create(
+                patient=patient,
+                drug=nuclearmedicinedrug,
+                radio_isotope=radioisotope,
+                requested_study=[study],
+            )
