76934053ae00c4c830f3340bab1c660d16771baf
TheRealVizard <vizard@divineslns.com>
2023-03-30 23:13:11 -0400

    Restored default permissions

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -11,4 +11,3 @@ class UserAdmin(UserAdmin):
     list_display = ("username", "email", "first_name", "last_name", "is_staff")
     list_filter = ("is_staff", "is_superuser", "is_active")
     search_fields = ("username", "first_name", "last_name", "email")
-
