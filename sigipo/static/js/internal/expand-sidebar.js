0e91ea4470bb4c0221654bf8f2bca6220ad91f1a
TheRealVizard <vizard@divineslns.com>
2023-04-01 14:16:38 -0400

    Added expand JS

---
+++ sigipo/static/js/internal/expand-sidebar.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    let link = $("main").data("path");
+    $(`a[href="${link}"]`).addClass("active-link");
+    $(`a[href="${link}"]`).parents("div.collapse").addClass("show");
+    $(`a[href="${link}"]`)
+        .parents("div.collapse").find("button.btn-toggle")
+        .attr("aria-expanded", true)
+        .removeClass("collapsed");
+});
