739a9dab015a297976997504620d719775418244
TheRealVizard <vizard@divineslns.com>
2023-04-01 15:57:14 -0400

    Updated font to Arial Nova, fixed html

--- sigipo/static/js/internal/expand-sidebar.js
+++ sigipo/static/js/internal/expand-sidebar.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-    let link = $("main").data("path");
-    $(`a[href="${link}"]`).addClass("active-link");
-    $(`a[href="${link}"]`).parents("div.collapse").addClass("show");
-    $(`a[href="${link}"]`)
-        .parents("div.collapse").find("button.btn-toggle")
-        .attr("aria-expanded", true)
-        .removeClass("collapsed");
+  let link = $("main").data("path");
+  $(`a[href="${link}"]`).addClass("active-link");
+  $(`a[href="${link}"]`).parents("div.collapse").addClass("show");
+  $(`a[href="${link}"]`)
+    .parents("div.collapse")
+    .find("button.btn-toggle")
+    .attr("aria-expanded", true)
+    .removeClass("collapsed");
 });
